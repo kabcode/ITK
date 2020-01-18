07adccbfa761709d1988c4ead4fcb6f7d0de581e
Gordian Kabelitz <kabcode@gmail.com>
2020-01-18 15:24:46 +0100

    Change to kabcode/RTK development branch

--- Modules/Remote/RTK.remote.cmake
+++ Modules/Remote/RTK.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(RTK
     "Reconstruction Toolkit (RTK) http://www.openrtk.org/"
   GIT_REPOSITORY ${git_protocol}://github.com/kabcode/RTK.git
-  GIT_TAG master
+  GIT_TAG development
 )
